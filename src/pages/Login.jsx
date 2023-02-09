58ab956e4efb3eae7b77373633cf427bfc1dd156
“smuel14” <smuel14.finca@gmail.com>
2023-02-09 17:04:30 -0500

    Pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+import '@styles/Login.scss';
+import Logo from '@logos/logo_yard_sale.svg'
+import { log } from 'mathjs';
+
+const Login = () => {
+	const form = useRef(null);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const formData = new FormData(form.current); //Traemos valores del formulario referenciado, escucha cada input
+		//Objeto a mandar a nuestro backend
+		const data = {
+			username : formData.get('email'),
+			password : formData.get('password')
+		}
+		console.log(data);
+	}
+
+	return (
+		<div className="Login">
+			<div className="Login-container">
+				<img src={Logo} alt="logo" className="logo" />
+				<form action="/" className="form" ref={form}>
+					<label htmlFor="email" className="label">Email address</label>
+					<input type="text" name="email" placeholder="platzi@example.cm" className="input input-email" />
+					<label htmlFor="password" className="label">Password</label>
+					<input type="password" name="password" placeholder="*********" className="input input-password" />
+					<button type="submit" className="primary-button login-button" onClick={handleSubmit}>
+						Log in
+					</button>
+					<a href="/">Forgot my password</a>
+				</form>
+				<button className="secondary-button signup-button">
+					Sign up
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
